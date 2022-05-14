de6147d494684a3b561491672dbf66e3b99972d8
EnoDucotterd <eno.ducotterd@studentfr.ch>
2022-05-14 15:36:45 +0200

    js lié et effectué

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var buttonhistory = document.querySelector(".more")
+var documentation1 = document.querySelector(".history-astro")
+var open = function(){
+  documentation1.classList.toggle("is-open")
+}
+buttonhistory.addEventListener("click",open)
+
