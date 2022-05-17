93a1fab83221d577cebd8139cf8d7286d9f9692c
EnoDucotterd <eno.ducotterd@studentfr.ch>
2022-05-17 14:56:44 +0200

    révision du javascript

--- js/index.js
+++ js/index.js
@@ -1,13 +1,8 @@
-var buttonhistory = document.querySelector(".more")
-var documentation1 = document.querySelector(".history-astro")
+var buttonastro = document.querySelector(".more")
+var documentation1 = document.querySelector(".history")
 var open = function(){
   documentation1.classList.toggle("is-open")
 }
-buttonhistory.addEventListener("click",open)
+buttonastro.addEventListener("click",open)
 
-var buttonglyphes = document.querySelector(".plus")
-var documentation2 = document.querySelector(".tetsujin-background")
-var open = function(){
-  documentation2.classList.toggle("is-open")
-}
-buttonglyphes.addEventListener("click",open)
+
